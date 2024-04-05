b33d2ebfbd310404f52fde9763ff43c8eba4686d
“Xhy177” <2645999638@qq.com>
2024-04-06 00:56:39 +0800

    4.6

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,16 @@
+class Car {
+    String color = "red";
+    int num = 4;
+    void show() {
+        System.out.println("color=" + color + ",num=" + num);
+    }
+}
 public class HelloWorld {
     public static void main(String[] args){
-        System.out.println("Hello,World!");
+        Car c1 = new Car(); //建立对象c1
+        Car c2 = new Car(); //建立对象c2
+        c1.color = "blue"; //对对象的属性进行修改
+        c1.show(); //使用对象的方法
+        c2.show();
     }
 }
